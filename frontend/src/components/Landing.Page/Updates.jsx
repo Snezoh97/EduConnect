a9fad8e07456ce5b97c7e41619632fd80498c993
Sinelizwi Ntaku <83240229+Snezoh97@users.noreply.github.com>
2023-11-21 12:02:04 +0200

    Update Updates.jsx

--- frontend/src/components/Landing.Page/Updates.jsx
+++ frontend/src/components/Landing.Page/Updates.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { updates, learn } from "../../assets";
-import MediaScreen from "./media";
+import MediaScreen from "./Media";
 import { Link } from "react-router-dom";
 
 const LatestUpdates = () => {
